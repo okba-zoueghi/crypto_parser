08cd9fab3bf032475555dd6edd22ba0bdba1a416
okba <okba.zoueghi@gmail.com>
2019-08-09 15:12:22 +0100

    Added ASN.1 documentation

--- parse_dhparam.h
+++ parse_dhparam.h
@@ -4,6 +4,14 @@
 
 #define DH_PARAM_BYTE_SIZE (DH_PARAM_BIT_SIZE / 8)
 
+/* Diffie Hellman parameters ASN.1 structure
+
+DHParameter ::= SEQUENCE {
+ prime INTEGER, -- p
+ base INTEGER, -- g
+ privateValueLength INTEGER OPTIONAL }
+*/
+
 typedef struct
 {
 	CP_UINT8 dhPrime[DH_PARAM_BYTE_SIZE];
--- parse_rsa.h
+++ parse_rsa.h
@@ -11,6 +11,22 @@ static const CP_UINT8 RSA_PUBLIC_KEY_OID[] = {0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D
 
 typedef enum {PKCS_1, PKCS_8_UNENCRYPTED} KeyFormat;
 
+/* RSA private key ASN.1 structure
+
+RSAPrivateKey ::= SEQUENCE {
+  version           Version,
+  modulus           INTEGER,  -- n
+  publicExponent    INTEGER,  -- e
+  privateExponent   INTEGER,  -- d
+  prime1            INTEGER,  -- p
+  prime2            INTEGER,  -- q
+  exponent1         INTEGER,  -- d mod (p-1)
+  exponent2         INTEGER,  -- d mod (q-1)
+  coefficient       INTEGER,  -- (inverse of q) mod p
+  otherPrimeInfos   OtherPrimeInfos OPTIONAL
+}
+*/
+
 typedef struct
 {
   CP_UINT8 modulus[MODULUS_BYTE_SIZE];
@@ -23,6 +39,14 @@ typedef struct
   CP_UINT8 iqmp[PRIME_SIZE];
   CP_UINT16 keyBitSize;
 }RsaPrivateKey;
+
+/* RSA public key ASN.1
+
+RSAPublicKey ::= SEQUENCE {
+    modulus           INTEGER,  -- n
+    publicExponent    INTEGER   -- e
+}
+*/
 
 typedef struct
 {
