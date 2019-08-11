d3c3a2df7d85cfa91cad6ac594726c595f8ead09
okba <okba.zoueghi@gmail.com>
2019-08-11 19:49:08 +0100

    Added ASN.1 documentation in parse_dsa.h

--- parse_dsa.h
+++ parse_dsa.h
@@ -4,7 +4,7 @@
 
 #define DSA_PARAM_BYTE_SIZE (DSA_PARAM_BIT_SIZE / 8)
 
-/* Digital Signature Algorithm ASN.1 structure
+/* Digital Signature Algorithm Parameters ASN.1 structure
 
 DSA-Parms  ::=  SEQUENCE  {
     p INTEGER,
@@ -23,4 +23,40 @@ typedef struct
 	CP_UINT16 gSize;
 }DsaParam;
 
+/* Digital Signature Algorithm Private Key structure
+Not standardized, the ASN.1 structure below is used by OpenSSL
+
+DSAPrivatKey_OpenSSL ::= SEQUENCE
+   version INTEGER,
+   p INTEGER,
+   q INTEGER,
+   g INTEGER,
+   y INTEGER,
+   x INTEGER
+}
+*/
+
+/* TO DO
+typedef struct
+{
+
+}DsaPrivateKey;
+*/
+
+/* Digital Signature Algorithm Public Key ASN.1 structure
+DSAPublicKey ::= INTEGER
+*/
+
+/* TO DO
+typedef struct
+{
+
+}DsaPublicKey;
+*/
+
 int parseDsaParam(CP_UINT8 * dsaParamDerInput, DsaParam * dsaParam);
+
+/* TO DO
+int parseDsaPrivateKey(CP_UINT8 * dsaPrivateKeyDerInput, DsaPrivateKey * dsaPrivateKey);
+int parseDsaPublicKey(CP_UINT8 * dsaPublicKeyDerInput, DsaPublicKey * dsaPublicKey);
+*/
