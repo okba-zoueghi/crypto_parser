acdd603041cd8b4bcdfad817cbe37b7f94a43d58
okba <okba.zoueghi@gmail.com>
2019-08-12 21:20:14 +0100

    Added license

--- parse_dsa.h
+++ parse_dsa.h
@@ -1,3 +1,25 @@
+/*
+  Copyright (C) 2019
+
+  Author: okba.zoueghi@gmail.com
+
+  This file is part of crypto_parser.
+
+  crypto_parser is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  crypto_parser is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with crypto_parser.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #include "cp_config.h"
 
 #define DSA_PARAM_BIT_SIZE 3072
